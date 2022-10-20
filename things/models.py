89cd4633264f54e2438b6a1d6ef40ba05aececaa
Joud Zeyadi <joud.zeyadi@kcl.ac.uk>
2022-10-20 21:00:57 +0100

    Final result, solution 1

--- things/models.py
+++ things/models.py
@@ -1,12 +1,9 @@
+from django.core.validators  import MinValueValidator,MaxValueValidator
+
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.db.models import Model
+from wsgiref.validate import validator
 
-class Thing(Model):
-    name = models.CharField()
-    description = models.TextField()
-    quantity = models.IntegerField()
-
-
-
-# Create your models here.
+class Thing(models.Model):
+    name=models.CharField(max_length=30,unique=True, blank=False)
+    description=models.CharField(max_length=120, blank=True)
+    quantity=models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)])
