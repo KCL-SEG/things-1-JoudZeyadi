87076154b8c0b44b5c2078927b355ca90bff2949
Joud Zeyadi <joud.zeyadi@kcl.ac.uk>
2022-10-20 19:15:24 +0100

    Exercise 2 first solution

--- things/models.py
+++ things/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import Model
+
+class Thing(Model):
+    name = models.CharField()
+    description = models.TextField()
+    quantity = models.IntegerField()
+
+
 
 # Create your models here.
